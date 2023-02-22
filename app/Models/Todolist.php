a93f109b61fdc34d5366540da7956b2df151e09d
davidrondina <rondinadavid.r@gmail.com>
2023-02-22 23:44:20 +0800

    Added create page, route config

--- app/Models/Todolist.php
+++ app/Models/Todolist.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Todolist extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        "title",
+        "description"
+    ];
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodolistController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [TodolistController::class, "index"])->name("index");
+Route::get('/create', [TodolistController::class, "create"])->name("create");
